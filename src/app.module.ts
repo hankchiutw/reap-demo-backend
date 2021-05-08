79f94ec78fb0dca4d366e7c8df5fe8438a735e09
hank <hankchiu.tw@gmail.com>
2021-05-08 17:21:52 +0800

    refactor: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApiModule } from './api';
 
 @Module({
   imports: [ApiModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
