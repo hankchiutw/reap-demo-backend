db21a8379705a266e00843e9fdd68ca8d3edade6
hank <hankchiu.tw@gmail.com>
2021-05-13 21:48:54 +0800

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get('API_PORT');
   if (!port) {
