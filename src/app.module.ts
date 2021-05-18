84e6d8ae2b7dc217af57c5998396f6b314498d65
hank <hankchiu.tw@gmail.com>
2021-05-18 21:39:55 +0800

    feat: add @nestjs/serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ErrorMessageInterceptor } from './core';
 import { ApiModule } from './api';
 import { OrmModule } from './orm';
@@ -10,6 +12,9 @@ import { OrmModule } from './orm';
     ConfigModule.forRoot({
       envFilePath: ['.env', '.env.default'],
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontent/dist/apps/instapic'),
     }),
     OrmModule.forRoot(),
     ApiModule,
